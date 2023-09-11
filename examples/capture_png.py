95b577557cd2fdd8811d9fdc324c0d6f2182cc06
bert13069598 <everglow83@naver.com>
2023-09-11 16:30:12 +0900

    catpure_png: update for save picture

--- examples/capture_png.py
+++ examples/capture_png.py
@@ -2,12 +2,16 @@
 
 # Capture a PNG while still running in the preview mode.
 
+from datetime import datetime
 import time
 
 from picamera2 import Picamera2, Preview
 
+current_time = datetime.now()
+formatted_time = current_time.strftime("%y%m%d_%H:%M")
+
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
+# picam2.start_preview(Preview.QTGL)
 
 preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
 picam2.configure(preview_config)
@@ -15,4 +19,4 @@ picam2.configure(preview_config)
 picam2.start()
 time.sleep(2)
 
-picam2.capture_file("test.png")
+picam2.capture_file(f"/home/bert/Pictures/{formatted_time}.png")
